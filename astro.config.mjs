3d0c3d31785b72dd0fcdbcef86025341c52f3918
ernocobain <ernocobain89@gmail.com>
2025-07-16 21:49:00 +0700

    perbaikan deploy ke cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 
+import cloudflare from '@astrojs/cloudflare';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.maunguli.com',
@@ -34,4 +37,5 @@ export default defineConfig({
   },
 
   integrations: [sitemap(), mdx()],
+  adapter: cloudflare(),
 });
